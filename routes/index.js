bc95ccf637b560e39c84f27aac545502d5309d3e
defectus <ondrej.linek@gmail.com>
2012-11-17 17:34:27 +0000

    mid work

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-
 /*
  * GET home page.
  */
+var moment = require('moment');
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function (req, res) {
+    res.render('index', {
+        title:'Oldřich Milostný, malířské práce',
+        flash:null,
+        status: 'Server status OK (@ ' + moment().format('hh:mm:ss')});
 };
