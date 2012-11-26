06c82a461aa542b7c2f6ecd9ef2bbe5dcd228f67
defectus <ondrej.linek@gmail.com>
2012-11-26 20:21:09 +0000

    refactoring

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ app.configure('production', function () {
 });
 
 app.get('/', routes.index);
+app.get('/gallery', routes.gallery);
 app.get('/map', map.show);
 app.get('/services', services.show);
 app.get('/contact', about.contact);
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
-/*
- * GET home page.
- */
 var moment = require('moment');
 
 exports.index = function (req, res) {
     res.render('index', {
+        title:'Oldřich Milostný, malířské práce',
+        flash:null,
+        status: 'Server status OK (@ ' + moment().format('hh:mm:ss')});
+};
+
+exports.gallery = function (req, res) {
+    res.render('gallery', {
         title:'Oldřich Milostný, malířské práce',
         flash:null,
         status: 'Server status OK (@ ' + moment().format('hh:mm:ss')});
